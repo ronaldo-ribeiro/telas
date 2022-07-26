547be085908650837509d6d9959bf53f1164f3f5
Ronaldo Ribeiro <ronaldo_ribeiro@outlook.com>
2022-07-26 18:23:42 -0300

    primeiro commit do projeto

---
+++ Telas/TealViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TealViewController.swift
+//  Telas
+//
+//  Created by Ronaldo Ribeiro on 26/07/22.
+//
+
+import UIKit
+
+class TealViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Teal Sreen"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Telas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Telas
+//
+//  Created by Ronaldo Ribeiro on 26/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
